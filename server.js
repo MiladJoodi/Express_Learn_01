56317436acf3485aaaf20763bc4b3008425a3e22
Milad Joodi <winbeta.com@gmail.com>
2024-08-04 22:09:02 +0330

    up

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose")
+
 
 app.get("/", (req, res)=>{
     res.status(200).json({msg: "blog server main page"});

d839166d0439f601ddf06b3d8ae0ce7199d018d9
Milad Joodi <winbeta.com@gmail.com>
2024-08-05 01:35:48 +0330

    Models

--- models/Post.js
+++ models/Post.js
@@ -3,5 +3,9 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
     title: {
         type: String
-    }
+    },
+    viewNum: {
+        type: Number,
+        required: true
+    },
 })
